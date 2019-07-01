79cdaf5c3192fefe80f4fb3200e08181be8e1648
zgh <zgh@192.168.0.105>
2019-07-01 16:11:11 +0800

    part 3

--- block.go
+++ block.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
+	"log"
 	"time"
 )
 
@@ -10,6 +13,32 @@ type Block struct {
 	Hash          []byte
 	Data          []byte
 	Nonce         int //Nonce 在对工作量证明进行验证时用到
+}
+
+// Serialize 将Block序列化一个字节数组
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
+
+// 将字节数组反序列化为一个Block
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	if err := decoder.Decode(&block); err != nil {
+		log.Panic(err)
+	}
+
+	return &block
+
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
